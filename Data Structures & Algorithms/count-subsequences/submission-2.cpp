95d4a8c26c63632c50a8b1789c69727795ab8e1d
Siddharth Singh <sidsingh11702@gmail.com>
2026-05-16 18:08:16 +0530

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    map<string,map<string,int>> dp;
+public:
+    int numDistinct(string s, string t) {
+        if (t.empty()) return dp[s][t] = 1;
+        if (s.empty()) return dp[s][t] = 0;
+        if (dp.count(s) && dp[s].count(t)) return dp[s][t];
+        if (s[0] == t[0]) return (dp[s.substr(1)][t] = numDistinct(s.substr(1),t)) + (dp[s.substr(1)][t.substr(1)] = numDistinct(s.substr(1),t.substr(1)));
+        return dp[s.substr(1)][t] = numDistinct(s.substr(1),t);
+    }
+};
