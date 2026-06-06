5a95f317c85d1f08523dd64647a7cf20c4963038
Siddharth Singh <sidsingh11702@gmail.com>
2026-06-06 17:19:12 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    void backtrack(int sum, int target, vector<int> curr, map<int,int>::iterator it, map<int,int>& cnt, vector<vector<int>>& res) {
+        if (sum == target) {
+            res.push_back(curr);
+            return;
+        }
+        if (it == cnt.end() || sum > target) return;
+        backtrack(sum,target,curr,next(it),cnt,res);
+        if (it->second > 0) sum += it->first, curr.push_back(it->first), it->second--, backtrack(sum,target,curr,it,cnt,res), it->second++;
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> curr; vector<vector<int>> res; map<int,int> cnt;
+        for (auto candidate : candidates) cnt[candidate]++;
+        backtrack(0,target,curr,cnt.begin(),cnt,res);
+        return res;
+    }
+};
