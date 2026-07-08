0e9174f3ad0a15c62f80a8e3c8de99010b0a21df
Siddharth Singh <sidsingh11702@gmail.com>
2026-07-08 15:00:42 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> inDegree(n,0), outDegree(n,0);
+        for (auto edge : trust) inDegree[edge[1]-1]++, outDegree[edge[0]-1]++;
+        int res = -1;
+        for (int i=0; i<n; i++) if (inDegree[i] == n-1 && outDegree[i] == 0) if (res == -1) res = i+1; else res = -1;
+        return res;
+    }
+};
